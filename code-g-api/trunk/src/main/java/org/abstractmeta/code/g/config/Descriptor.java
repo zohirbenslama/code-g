849c11c2750a62daa327e197d55a265f0d565ea1
abstractmeta <abstractmeta@7b257d32-9133-9ba5-60da-1b10c5724092>
2012-05-20 06:47:20 +0000

    added apache licences, integrated with sonatype

--- code-g-api/trunk/src/main/java/org/abstractmeta/code/g/config/Descriptor.java
+++ code-g-api/trunk/src/main/java/org/abstractmeta/code/g/config/Descriptor.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright 2011 Adrian Witas
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.abstractmeta.code.g.config;
 
 import java.util.Collection;
@@ -6,7 +21,7 @@ import java.util.Map;
 import java.util.Set;
 
 /**
-* Represents code generator configuration descriptor
+* Represents code plugin configuration descriptor
 *
 * @author Adrian Witas
 */
@@ -93,7 +108,7 @@ public interface Descriptor {
 
 
     /**
-     * Current project packages used to compile auto generator sources.
+     * Current project packages used to compile auto plugin sources.
      * To avoid mixing up auto generated code with the actual code,
      * The generation source may depend on some packages with in the same problem.
      * This is not issue when source code is a separate maven project.
