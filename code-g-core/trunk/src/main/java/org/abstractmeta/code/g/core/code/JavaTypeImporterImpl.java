8be98a23b4e271c4942ee97ff870d435cc73bb89
abstractmeta <abstractmeta@7b257d32-9133-9ba5-60da-1b10c5724092>
2012-05-20 16:28:40 +0000

    added apache license

--- code-g-core/trunk/src/main/java/org/abstractmeta/code/g/core/code/JavaTypeImporterImpl.java
+++ code-g-core/trunk/src/main/java/org/abstractmeta/code/g/core/code/JavaTypeImporterImpl.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright 2011 Adrian Witas
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.abstractmeta.code.g.core.code;
 
 import org.abstractmeta.code.g.code.JavaTypeImporter;
@@ -150,7 +165,7 @@ public class JavaTypeImporterImpl implements JavaTypeImporter {
 
 
     protected void addTypeName(String name) {
-        if (name.equals("org.abstractmeta.code.g.core.generator.BuilderClassGeneratorTest.Bar")) {
+        if (name.equals("org.abstractmeta.code.g.core.plugin.BuilderClassGeneratorTest.Bar")) {
             throw new IllegalStateException("!!!");
         }
         typeNames.add(name);
--- code-g-core/trunk/src/main/java/org/abstractmeta/code/g/core/plugin/AbstractGeneratorPlugin.java
+++ code-g-core/trunk/src/main/java/org/abstractmeta/code/g/core/plugin/AbstractGeneratorPlugin.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright 2011 Adrian Witas
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.abstractmeta.code.g.core.plugin;
 
 import org.abstractmeta.code.g.code.JavaType;
@@ -14,7 +29,7 @@ import java.lang.reflect.Type;
 import java.util.*;
 
 /**
- * Abstract generator plugin.
+ * Abstract plugin plugin.
  * <p>Convenience class that provides default implementation for most core cases.</p>
  * <h3>Plugin configuration</h3>
  * The following naming convention is used to configure a plugin: <b>
@@ -30,7 +45,7 @@ import java.util.*;
  * <h3>Plugin usage</h3>
  * Each sub class should implement the following methods
  * <ul>
- * <li>isApplicable - filters source classes by checking if plugin generator can applied</li>
+ * <li>isApplicable - filters source classes by checking if plugin plugin can applied</li>
  * <li>generateType - generates new type based on a given source type</li>
  * <p/>
  * </ul>
@@ -140,7 +155,7 @@ public abstract class AbstractGeneratorPlugin {
 
 
     /**
-     * Is a given java source type applicable to this class generator plugin.
+     * Is a given java source type applicable to this class plugin plugin.
      *
      * @param sourceType java source type
      * @return true if applicable
