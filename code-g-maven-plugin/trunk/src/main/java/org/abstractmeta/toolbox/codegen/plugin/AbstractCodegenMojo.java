e6e796e1edcef858f86b1b55b07089c174fd9f98
abstractmeta <abstractmeta@7b257d32-9133-9ba5-60da-1b10c5724092>
2012-05-20 17:25:47 +0000

    added licencse, integrated with maven central

--- code-g-maven-plugin/trunk/src/main/java/org/abstractmeta/toolbox/codegen/plugin/AbstractCodegenMojo.java
+++ code-g-maven-plugin/trunk/src/main/java/org/abstractmeta/toolbox/codegen/plugin/AbstractCodegenMojo.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright 2011 Adrian Witas
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.abstractmeta.toolbox.codegen.plugin;
 
 
@@ -61,7 +76,7 @@ public abstract class AbstractCodegenMojo extends AbstractMojo {
         try {
 
             List<String> classPathEntries = getClassPathEntries();
-            getLog().debug("unit generator class path " + classPathEntries);
+            getLog().debug("unit plugin class path " + classPathEntries);
             UnitGenerator unitGenerator = new UnitGeneratorImpl();
             if(units == null) {
                 throw new IllegalStateException("units was null");
--- code-g-maven-plugin/trunk/src/main/java/org/abstractmeta/toolbox/codegen/plugin/Descriptor.java
+++ code-g-maven-plugin/trunk/src/main/java/org/abstractmeta/toolbox/codegen/plugin/Descriptor.java
@@ -16,18 +16,13 @@
 package org.abstractmeta.toolbox.codegen.plugin;
 
 import org.abstractmeta.code.g.core.config.DescriptorImpl;
-import org.abstractmeta.code.g.core.plugin.ClassGeneratorPlugin;
 
 /**
- * Represents ClassGenerator
+ * Represents Descriptor
  * <p>
  * </p>
  *
  * @author Adrian Witas
  */
-public class ClassGenerator extends DescriptorImpl{
-    @Override
-    public String getPlugin() {
-        return ClassGeneratorPlugin.class.getName();
-    }
+public class Descriptor extends DescriptorImpl {
 }
